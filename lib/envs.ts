27110b9f8d6b8fc6e7fb48a609b9a15a57f725e2
mystarlock <baoanhto1111@gmail.com>
2025-06-02 20:36:38 +1000

    fix: add OPEN_API_KEY access to envs.ts

--- lib/envs.ts
+++ lib/envs.ts
@@ -2,5 +2,6 @@ import { EnvKey } from "@/types/key-type"
 
 // returns true if the key is found in the environment variables
 export function isUsingEnvironmentKey(type: EnvKey) {
+  console.log("Checking key: ", type, "->", process.env[type])
   return Boolean(process.env[type])
 }
