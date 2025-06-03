43063cd438f7edf2d8224b708f180326d35d9ab9
mystarlock <baoanhto1111@gmail.com>
2025-06-03 14:36:45 +1000

    Export OpenAI API key

--- lib/envs.ts
+++ lib/envs.ts
@@ -1,5 +1,5 @@
 import { EnvKey } from "@/types/key-type"
-
+export const OPEN_API_KEY = process.env.OPEN_API_KEY
 // returns true if the key is found in the environment variables
 export function isUsingEnvironmentKey(type: EnvKey) {
   console.log("Checking key: ", type, "->", process.env[type])
